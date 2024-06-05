bcefa7c9640f8e957a9d355a5e5dcc52e46d40c3
brianngumbau <brianngumbau9075@gmail.com>
2024-06-04 23:24:56 -0700

    update

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -20,7 +20,7 @@ def close_db(error):
 
 @app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
-    """ HBNB is alive! """
+    """ HBNB IS WORKING"""
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
